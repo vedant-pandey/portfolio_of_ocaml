0c959158cc2a31c4412a1d1f2617561274161206
Vedant <vedantpandey46@gmail.com>
2024-11-27 22:18:19 +0530

    Init: First commit

---
+++ lib/mantra/Mantra.ml
@@ -0,0 +1,40 @@
+open! Core
+
+(* INFIX	::=	(CORE ∣ % ∣ <) { OPERATOR } *)
+(*  	∣	 # { OPERATOR }+ *)
+(* PREFIX	::=	! { OPERATOR } *)
+(*  	∣	 (? ∣ ~) { OPERATOR }+ *)
+(* OPERATOR	::=	~ ∣ ! ∣ ? ∣ CORE ∣ % ∣ < ∣ : ∣ . *)
+(* CORE	::=	$ ∣ & ∣ * ∣ + ∣ - ∣ / ∣ = ∣ > ∣ @ ∣ ^ ∣ | *)
+
+(* "$"  - *)
+(* "&"  - *)
+(* "*"  - *)
+(* "+"  - *)
+(* "-"  - *)
+(* "/"  - *)
+(* "="  - *)
+(* ">"  - *)
+(* "@"  - *)
+(* "^"  - *)
+(* "|"  - *)
+(* "%"  - *)
+(* "<"  - *)
+
+(* List -  *)
+(* Result - *)
+(* Option - *)
+(* Map -  *)
+(* File - *)
+(* String - *)
+(* Print - *)
+(* Array -  *)
+
+(* .map *)
+(* .fold *)
+(* .init *)
+
+module Mantra = struct
+  (* List.bind *)
+  (* let ( =>) *)
+end
